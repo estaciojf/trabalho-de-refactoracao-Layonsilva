7bf0ce990f1cedc6bd34101d233aa671f6f93bff
Layonsilva <65149208+Layonsilva@users.noreply.github.com>
2020-06-16 00:46:09 -0300

    Add files via upload

--- src/Enum/EDataBase.java
+++ src/Enum/EDataBase.java
@@ -1,8 +1,11 @@
 package Enum;
 
 public abstract class EDataBase {
-
+	/**
+	 * String de conexão alterada.
+	 *
+	 */
 	public static final String DB_USER = "root";
-	public static final String DB_PASS = "yami";
-	public static final String DB_URL = "jdbc:mysql://localhost:3306/locadora";
+	public static final String DB_PASS = " ";
+	public static final String DB_URL = "\"jdbc:mysql://localhost:3306/tblRefacLoc\""; 
 }
